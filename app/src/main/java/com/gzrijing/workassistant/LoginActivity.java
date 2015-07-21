318765a61b2e3913331a2d9c60819678dc4edce7
me <13425090925@163.com>
2015-07-21 14:04:10 +0800

    new login

--- app/src/main/java/com/gzrijing/workassistant/LoginActivity.java
+++ app/src/main/java/com/gzrijing/workassistant/LoginActivity.java
@@ -1,15 +1,141 @@
 package com.gzrijing.workassistant;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private EditText et_user;
+    private EditText et_pwd;
+    private Button btn_login;
+    private ImageView iv_user;
+    private ImageView iv_pwd;
+    private ImageView iv_memory;
+    private boolean flag;
+    private String isMemory; // isMemory变量用来判断SharedPreferences有没有数据
+    private String file = "saveUserAndPwd";// 用于保存SharedPreferences的文件
+    private SharedPreferences sp; // 声明一个SharedPreferences
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+        initData();
+        setListeners();
+    }
+
+    private void initData() {
+        getUserNamePwd();
+    }
+
+    private void getUserNamePwd() {
+        sp = getSharedPreferences(file, MODE_PRIVATE);
+        isMemory = sp.getString("isMemory", "no");
+        // 进入界面时，这个if用来判断SharedPreferences里面user和pwd有没有数据，
+        // 有的话则直接打在EditText上面
+        if (isMemory.equals("yes")) {
+            String user = sp.getString("user", "");
+            String pwd = sp.getString("pwd", "");
+            et_user.setText(user);
+            et_pwd.setText(pwd);
+            iv_memory.setImageResource(R.drawable.login_checkbox_on);
+            flag = true;
+        }
+    }
+
+    private void initViews() {
+        et_user = (EditText) findViewById(R.id.login_user_et);
+        iv_user = (ImageView) findViewById(R.id.login_user_iv);
+        et_pwd = (EditText) findViewById(R.id.login_password_et);
+        iv_pwd = (ImageView) findViewById(R.id.login_password_iv);
+        btn_login = (Button) findViewById(R.id.login_login_btn);
+        iv_memory = (ImageView) findViewById(R.id.login_checkbox_iv);
+    }
+
+    private void setListeners() {
+        btn_login.setOnClickListener(this);
+        iv_memory.setOnClickListener(this);
+
+        et_user.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    iv_user.setImageResource(R.drawable.login_user_on);
+                }else{
+                    iv_user.setImageResource(R.drawable.login_user_off);
+                }
+            }
+        });
+
+        et_pwd.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    iv_pwd.setImageResource(R.drawable.login_pwd_on);
+                }else{
+                    iv_pwd.setImageResource(R.drawable.login_pwd_off);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_login_btn:
+                login();
+                break;
+
+            case R.id.login_checkbox_iv:
+                if (flag) {
+                    iv_memory.setImageResource(R.drawable.login_checkbox_off);
+                } else {
+                    iv_memory.setImageResource(R.drawable.login_checkbox_on);
+                }
+                flag = !flag;
+                break;
+        }
+    }
+
+    private void login() {
+
+    }
+
+    @Override
+    protected void onStop() {
+        remember();
+        super.onStop();
+    }
+
+    // remember方法用于判断是否记住密码，checkBox选中时，提取出EditText里面的内容，
+    // 放到SharedPreferences里面的user和pwd中
+    private void remember() {
+        if (flag) {
+            if (sp == null) {
+                sp = getSharedPreferences(file, MODE_PRIVATE);
+            }
+            Editor edit = sp.edit();
+            edit.putString("user", et_user.getText().toString().trim());
+            edit.putString("pwd", et_pwd.getText().toString().trim());
+            edit.putString("isMemory", "yes");
+            edit.commit();
+        } else {
+            if (sp == null) {
+                sp = getSharedPreferences(file, MODE_PRIVATE);
+            }
+            SharedPreferences.Editor edit = sp.edit();
+            edit.putString("isMemory", "no");
+            edit.commit();
+        }
     }
 
 }
